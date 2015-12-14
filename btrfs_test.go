db6e6322efa2d468767ad95204f937df374b2ded
Vincent Batts <vbatts@hashbangbash.com>
2015-12-14 17:31:53 -0500

    btrfs: testing subvolume create
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- btrfs_test.go
+++ btrfs_test.go
@@ -1,10 +1,14 @@
 package btrfs
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
+	"syscall"
 	"testing"
 
 	"./fs"
+	"./loop"
 )
 
 func TestIsSubvolume(t *testing.T) {
@@ -15,7 +19,39 @@ func TestIsSubvolume(t *testing.T) {
 	t.Log(b)
 }
 
-func testingImage() (string, error) {
+func TestSubvolume(t *testing.T) {
+	bf, err := backingFile()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(bf)
+	lh, err := loop.AttachLoopDevice(bf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer lh.Close()
+
+	tmpdir, err := ioutil.TempDir("", "btrfs-testing.")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	err = syscall.Mount(lh.Name(), tmpdir, "btrfs", uintptr(syscall.MS_NOATIME), "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer syscall.Unmount(tmpdir, 0)
+
+	for i := 0; i < 10; i++ {
+		if err := SubvolCreate(tmpdir, fmt.Sprintf("vol-%d", i)); err != nil {
+			t.Errorf("failed to create vol-%d : %s", i, err)
+		}
+	}
+}
+
+// helper
+func backingFile() (string, error) {
 	fh, err := ioutil.TempFile("", "btrfs-testing.")
 	if err != nil {
 		return "", err
